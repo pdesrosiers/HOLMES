e15edf4915dd7835270189b53278d34276d41584
Patrick Desrosiers <patrick@MacBook-Pro-de-Patrick.local>
2020-09-24 09:33:00 -0400

    wrong file paths fixed

--- generative_model/example2.py
+++ generative_model/example2.py
@@ -3,20 +3,41 @@
 
 ##############################
 # Step 0: Prepare analysis
+
+# Load packages
 from tools import *
+import os
 
 # Set parameters
-observations = 1000
-alpha = 0.01
-build_sc = False
+observations = 1000 # number of samples, i.e., number of data points or number of sites
+alpha = 0.01 # significance level
 
-# Define the simplicial complex of interest
+# Choose the name of the directory (dir_name) where to save the files and the 'prefix' name of each 
+# created files (data_name)
+dir_name = 'synthetic_data'
+data_name = 'data'
+
+# Create target Directory if doesn't exist
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+    print("Directory ", dir_name, " Created ")
+else:
+    print("Directory ", dir_name, " already exists")
+
+data_name = os.path.join(dir_name, data_name)
+
+##############################
+# Step 1: Define the higher-order structure of interest
+
+# List of 1-simplices
 vp_1simp = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3], [3, 4], [3, 5], [4, 5], [5, 6], [5, 7], [6, 7], [4, 8], [7, 8], [7, 9], [8, 9]]
+# List of 2-simplices
 vp_2simp = [[0, 1, 2], [1, 2, 3], [0, 1, 3], [0, 2, 3],  [7, 8, 9]]
+# Minimal list of simplices.  This defines a simplicial complex
 facet_list = [[3, 4], [3, 5], [4, 5], [5, 6], [5, 7], [6, 7], [4, 8],[0, 1, 2], [1, 2, 3], [0, 1, 3], [0, 2, 3],  [7, 8, 9]]
 
 ##############################
-# Step 1: Find a good factor graph
+# Step 2: Find a good factor graph
 
 # The next loop finds a factor graph without induced interactions (interactions that we didn't specify)
 # TODO Even though that's the goal of  this loop, I think there are missing steps. For instance, we should also
@@ -25,7 +46,7 @@ i = 1
 switch = True
 while switch:
     print(i)
-    factorgraph = FactorGraph(facet_list, N=observations, alpha=alpha, build_sc=build_sc)
+    factorgraph = FactorGraph(facet_list, N=observations, alpha=alpha, build_sc=False)
 
     fg_1simplices_list, fg_2simplices_list = get_fg_vp_pathless(factorgraph, observations, alpha)
 
@@ -53,20 +74,20 @@ print(fg_1simplices_list)
 print(fg_2simplices_list)
 
 # Save factor graph
-with open(r'Directory\fg.pkl', 'wb') as output:
+with open(data_name + '_fg.pkl', 'wb') as output:
     pickle.dump(factorgraph, output, pickle.HIGHEST_PROTOCOL)
 
-with open(r'Directory\fg.pkl', 'rb') as fg_file:
+with open(data_name + '_fg.pkl', 'rb') as fg_file:
     factorgraph = pickle.load(fg_file)
     probdist = Prob_dist(factorgraph)
 
     
 ##############################
-# Step 2: Generated synthetic data  
+# Step 3: Generate synthetic data  
 
-#The next loop generates k Biadjacency matrices AKA presence/absence matrices.
-#The variable observations gives the number of sites we want to generate for each matrix.       
-k = 3
+# The next loop generates k Biadjacency matrices AKA presence/absence matrices.
+# The variable observations gives the number of sites we want to generate for each matrix.       
+k = 2
 for i in np.arange(0, k, 1):
     state = np.random.randint(2, size=len(factorgraph.node_list))
     print(state)
@@ -77,4 +98,4 @@ for i in np.arange(0, k, 1):
     sampler.sample(observations)
     
     bipartite = build_bipartite(sampler.results['sample'])
-    np.save(r'Directory\bipartite_' + str(i), bipartite)    
+    np.save(data_name + '_bipartite_' + str(i), bipartite)    
