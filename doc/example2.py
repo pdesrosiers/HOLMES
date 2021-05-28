2447c3a5e1ced34a62ca6b7391d2a7cffa733630
Xavier <xavier.roy-pomerleau.1@ulaval.ca>
2021-05-28 13:00:43 -0400

    Updated the example to work with updated core functions

--- doc/example2.py
+++ doc/example2.py
@@ -7,15 +7,22 @@
 # Load packages
 from holmes.generative_model.tools import *
 import os
+from collections import Counter
 
 # Set parameters
-building_observations = 1000 #Number used by methods of the FactorGraph to find appropriate factors for each facet.
+building_observations = 100000 #Number used by methods of the FactorGraph to find appropriate factors for each facet.
                              #This number can differ from the number of observations we want to generate, but it seems
                              #right to make it greater than (or equal to) the number of samples
 observations = 1000 # number of samples, i.e., number of data points or number of sites
 alpha = 0.01 # significance level
 build_sc = True # build a simplicial complex instead of a hypergraph
-building_constraint = 'None' #Can either be 'None', 'Soft' or 'Hard'. See documentation of the FactorGraph class
+building_constraint = 'None' #Can either be 'None', 'Soft' or 'Hard'.
+                             #'None' : the algorithm will try to respect the facet list, but might destroy
+                             #specified dependencies and induce new ones.
+                             #'Soft' : the effective facet list will contain all dependencies specified in
+                             #facet_list, but might also contain induced dependencies.
+                             #'Hard' : the effective facet list will be identical to the specified facet_list
+
 
 # Choose the name of the directory (dir_name) where to save the files and the 'prefix' name of each 
 # created files (data_name)
@@ -35,14 +42,14 @@ data_name = os.path.join(dir_name, data_name)
 # Step 1: Define the higher-order structure of interest
 
 # Minimal list of simplices.  This defines a simplicial complex
-facet_list = [[3, 4], [3, 5], [4, 5], [5, 6], [5, 7], [6, 7], [4, 8],[0, 1, 2], [1, 2, 3], [0, 1, 3], [0, 2, 3],  [7, 8, 9]]
+#facet_list = [[3, 4], [3, 5], [4, 5], [5, 6], [5, 7], [6, 7], [4, 8],[0, 1, 2], [1, 2, 3], [0, 1, 3], [0, 2, 3],  [7, 8, 9]]
 
 #You can either specified a facet_list, or have the following function generate one. The first parameter is the number
 #of nodes in the simplicial complex and the second is a list a proportions. The first proportion indicates the
 #proportion of facets of size 3 out of N choose 3 possibilities and the second indicates the proportion of facets of
 #size 2 out of N choose 2 possibilities
 
-#facet_list = generate_facet_list_proportions(10, [0.1, 0.5])
+facet_list = generate_facet_list_proportions(10, [0.01, 0.5])
 
 ##############################
 # Step 2: Find a good factor graph
@@ -71,6 +78,6 @@ for i in np.arange(0, k, 1):
     proposer = BitFlipProposer(factorgraph, energy_obj, state)
     sampler = Sampler(proposer, temperature=1, initial_burn=5, sample_burn=1)
     sampler.sample(observations)
-    
+
     bipartite = build_bipartite(sampler.results['sample'])
     np.save(data_name + '_bipartite_' + str(i), bipartite)
