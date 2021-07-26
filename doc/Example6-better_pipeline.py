98d5dc48cc7568775cb008d14e90a4d792a0cfb8
Xavier <xavier.roy-pomerleau.1@ulaval.ca>
2021-07-26 17:14:54 -0400

    Pipeline that goes with the new function for triangles

---
+++ doc/Example6-better_pipeline.py
@@ -0,0 +1,123 @@
+##############################
+# This is a simple script that explains how to infer a simplicial complex or a hypergraph
+# from a presence/absence data table, i.e., a binary matrix of dimension (pun), where p
+# is the population size (number of random variables) and n is the sample size (number of # data points)
+
+##############################
+# Step 0: Prepare the analysis
+
+print("\nStep 0: Load modules and data and set options")
+
+# Load the module for the inference with asymptotic statistical tests
+from holmes.data_analysis.asymptotic_significative_interactions import *
+import pandas as pd
+
+
+# Set the pption to decide if we use the step method (recommended) or the systematic method, which is
+# longer and does not return a simplicial complex. Use step_method = False for the systematic method.
+
+step_method = True
+
+# Choose the name of the directory (dir_name) where to save the files and the 'prefix' name of each
+# created files (data_name)
+
+dir_name = 'results'
+data_name = 'crime-betterpipe'
+
+# Create target Directory if doesn't exist
+
+if not os.path.exists(dir_name):
+    os.mkdir(dir_name)
+    print("Directory ", dir_name, " Created ")
+else:
+    print("Directory ", dir_name, " already exists")
+
+data_name = os.path.join(dir_name, data_name)
+
+# Choose the significance level alpha to use throughout the analysis.
+alpha = 0.01
+
+df = pd.read_csv('out.moreno_crime_crime', sep=' ', skiprows=2, header=None)
+
+print(df[0])
+
+g = df.groupby(0)[1].apply(list).reset_index()
+
+print(g)
+
+k = pd.get_dummies(g[1].apply(pd.Series).stack()).sum(level=0)
+
+print(k)
+
+data_matrix = k.to_numpy()
+
+print(data_matrix)
+
+
+p,n = data_matrix.shape
+print("Preparing to analyze a data set with " + str(p) + " variables and " + str(n) + " samples")
+
+##############################
+# First step: Extract all the unique tables
+
+print("\nStep 1: Extract all the unique tables")
+
+# Finds all unique tables
+find_unique_tables(data_matrix, data_name)
+
+print("Unique contingency tables saved in file " + data_name + "_table_list.json")
+
+##############################
+# Second step: Extract all the pvalues with an asymptotic distribution
+
+print('\nStep 2: Extract pvalues for all tables with an asymptotic distribution')
+
+pvalues_for_tables(data_name)
+
+print("\nResulting p-values saved in file " + data_name + "_asymptotic_pval_dictio.json")
+
+##############################
+# Third step: Find table for all links and their associated pvalue
+
+print('\nStep 3 : Find table for all links and their associated pvalue')
+
+with open(data_name + '_asymptotic_pval_dictio.json') as jsonfile:
+    dictio = json.load(jsonfile)
+    save_pairwise_p_values_phi_dictionary(data_matrix, dictio, data_name + '_asymptotic_pvalues')
+
+print("Results saved in file " + data_name + "_asymptotic_pvalues.csv")
+
+##############################
+# Fourth step: Choose alpha and extract the network
+
+print('\nStep 4: Generate network and extract edge_list for a given alpha')
+
+g = read_pairwise_p_values(data_name + '_asymptotic_pvalues.csv', alpha)
+
+nx.write_edgelist(g, data_name + '_asymptotic_edge_list_' + str(alpha)[2:] + '.txt', data=True)
+
+print('Number of nodes : ', g.number_of_nodes())
+print('Number of links : ', g.number_of_edges())
+print("Edge list saved in file " + data_name + "_asymptotic_edge_list_" + str(alpha)[2:] + ".txt")
+
+##############################
+# Seventh step: Find all triangles in the previous network
+
+print('\nStep 7: Find all empty triangles in the network')
+
+g = read_pairwise_p_values(data_name + '_asymptotic_pvalues.csv', alpha)
+
+save_all_triangles(g, data_name + '_asymptotic_triangles_' + str(alpha)[2:])
+
+print('Number of triangles : ', count_triangles_csv(data_name + '_asymptotic_triangles_' + str(alpha)[2:] + '.csv'))
+
+triangles_p_values(data_name, data_name + '_asymptotic_triangles_' + str(alpha)[2:] + '.csv', data_name + '_asymptotic_triangles_' + str(alpha)[2:] + '_pvalues.csv', data_matrix)
+
+##############################
+# Last step: Exctract all 2-simplices
+
+print("\nStep 9: Extract all 2-simplices")
+
+significant_triplet_from_csv(data_name + '_asymptotic_triangles_' + str(alpha)[2:] + '_pvalues.csv', alpha, data_name + '_asymptotic_2-simplices_' + str(alpha)[2:])
+
+print("2-simplices saved in " + data_name + "_asymptotic_2-simplices.json")
